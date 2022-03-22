a748aaf28ce78bff9a673f22f68cc5147e02de01
Carolig19 <ciribarrenguerra@gmail.com>
2022-03-22 21:43:20 +0000

    react

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,10 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+import { useState } from "react";
+import index from "/workspace/react-hello/src/styles/index.css";
 const Home = () => {
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="container"></div>
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,43 @@
-//import react into the bundle
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-
-// include your styles into the webpack bundle
 import "../styles/index.css";
-
-//import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+export default function ToDo() {
+	const [Tarea, SetTarea] = useState("");
+	const [Lista, SetLista] = useState([]);
+
+	return (
+		<div className="container">
+			<div className="titulo">
+				<h1>To Do List</h1>
+			</div>
+			<input
+				onChange={(e) => SetTarea(e.target.value)}
+				value={Tarea}
+				placeholder="Nueva Tarea"></input>
+			<button onClick={() => SetLista([...Lista, Tarea])}>Add</button>
+
+			<div>
+				{Lista.map((Tarea, i) => {
+					return (
+						<p key={i}>
+							{Tarea}
+							<button
+								className="delete"
+								onClick={() => {
+									SetLista(
+										Lista.filter(
+											(Lista, indice) => indice !== i
+										)
+									);
+								}}></button>
+						</p>
+					);
+				})}
+			</div>
+		</div>
+	);
+}
+
+ReactDOM.render(<ToDo />, document.querySelector("#app"));

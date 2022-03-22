41bcf8684d368d13ba8d7ce94d9424a67587719b
Carolig19 <ciribarrenguerra@gmail.com>
2022-03-22 23:29:17 +0000

    fetch

--- src/js/index.js
+++ src/js/index.js
@@ -1,41 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import "../styles/index.css";
-import Home from "./component/home.jsx";
 
 export default function ToDo() {
-	const [Tarea, SetTarea] = useState("");
-	const [Lista, SetLista] = useState([]);
+	const [Tarea, SetTarea] = useState([]);
 
+	useEffect = (() => {
+		var raw = "";
+		var requestOptions = {
+			method: "GET",
+			body: raw,
+			redirect: "follow",
+		};
+
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/Carolig19",
+			requestOptions
+		)
+			.then((response) => response.json())
+			.then((result) => {
+				console.log(result);
+				SetTarea(result);
+			})
+			.catch((error) => console.log("error", error));
+	},[]);
 	return (
 		<div className="container">
 			<div className="titulo">
 				<h1>To Do List</h1>
 			</div>
-			<input
-				onChange={(e) => SetTarea(e.target.value)}
-				value={Tarea}
-				placeholder="Nueva Tarea"></input>
-			<button onClick={() => SetLista([...Lista, Tarea])}>Add</button>
-
-			<div>
-				{Lista.map((Tarea, i) => {
-					return (
-						<p key={i}>
-							{Tarea}
-							<button
-								className="delete"
-								onClick={() => {
-									SetLista(
-										Lista.filter(
-											(Lista, indice) => indice !== i
-										)
-									);
-								}}></button>
-						</p>
-					);
-				})}
-			</div>
+		<div>{Tarea?map=((e,i)=>{
+			return <li key={i}>{e.label}</li>
+		})
+		</div>
 		</div>
 	);
 }
